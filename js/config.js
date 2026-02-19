988088db6ab5df9a5700065c2055a9bfee5ffc08
Alubiama <alubiama@gmail.com>
2026-02-20 00:26:55 +0300

    feat: add Firebase config for multiplayer

---
+++ js/config.js
@@ -0,0 +1,10 @@
+// Firebase Configuration
+// DO NOT commit this file to git!
+
+window.FIREBASE_API_KEY = "AIzaSyAJ5q-WgKtu897eG5o5pCfsuq-HaobguZw";
+window.FIREBASE_AUTH_DOMAIN = "tictactoe-1491f.firebaseapp.com";
+window.FIREBASE_DATABASE_URL = "https://tictactoe-1491f-default-rtdb.europe-west1.firebasedatabase.app";
+window.FIREBASE_PROJECT_ID = "tictactoe-1491f";
+window.FIREBASE_STORAGE_BUCKET = "tictactoe-1491f.firebasestorage.app";
+window.FIREBASE_MESSAGING_SENDER_ID = "642012786258";
+window.FIREBASE_APP_ID = "1:642012786258:web:59bc177efea95133d0398b";
